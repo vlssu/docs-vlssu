bff2ed7740219b8f589d326eab4209dafb703319
vlssu <vlssu@users.noreply.github.com>
2024-07-02 12:49:45 +0000

    deploy: a479f2675e8d7185ac4181930f6fef2f7e6ff5a7

--- assets/js/8da58278.39ce871d.js
+++ assets/js/8da58278.39ce871d.js
@@ -36,8 +36,8 @@ const metadata = {
   "editUrl": "https://github.com/vlssu/docs.vlssu.com/blob/master/docs/mcserver/picture.md",
   "tags": [],
   "version": "current",
-  "lastUpdatedBy": "飒爽师叔",
-  "lastUpdatedAt": 1719924219000,
+  "lastUpdatedBy": "vlssu",
+  "lastUpdatedAt": 1719924494000,
   "sidebarPosition": 7,
   "frontMatter": {
     "sidebar_position": 7
